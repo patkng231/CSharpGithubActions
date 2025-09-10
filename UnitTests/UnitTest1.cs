cd648008acd8906b463c2a7c218c998bd7c01a09
Patrick <Patrick@ROG-DESKTOP>
2025-09-09 22:41:59 -0400

    Added inline test

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace UnitTests
         }
 
         [Theory]
-        [InlineData(1, 3, 4)]
+        [InlineData(1, 3, 4)] 
         [InlineData(2, 3, 5)]
         public void Test2(int x, int y, int result)
         {
@@ -21,6 +21,7 @@ namespace UnitTests
         [Theory]
         [InlineData(5, 5, 25)]
         [InlineData(5, 0, 0)]
+        [InlineData(1, 1, 1)]
         public void Test3(int x, int y, int result)
         {
             Assert.True(x * y == result);
